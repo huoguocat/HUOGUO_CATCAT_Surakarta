5770eb775da5560d215318136b71534002ad66f3
huoguocat <3030916181@qq.com>
2024-03-22 17:22:18 +0800

    AI 1.5

--- src/surakarta/surakarta_agent/surakarta_agent_mine.cpp
+++ src/surakarta/surakarta_agent/surakarta_agent_mine.cpp
@@ -15,6 +15,9 @@ SurakartaMove SurakartaAgentMine::CalculateMove() {
     SurakartaMove none_eat_move({0, 0}, {0, 0}, game_info_->current_player_);
     SurakartaMove bad_move({0, 0}, {0, 0}, game_info_->current_player_);
     SurakartaMove ran_move({0, 0}, {0, 0}, game_info_->current_player_);
+    SurakartaMove fis_bad_move({0, 0}, {0, 0}, game_info_->current_player_);
+    SurakartaMove fis_ran_move({0, 0}, {0, 0}, game_info_->current_player_);
+    SurakartaMove fis_last_move({0, 0}, {0, 0}, game_info_->current_player_);
     srand((unsigned)time(NULL));
     unsigned int which_ran = 1 + (rand() % 9);  // 随机种子防止困顿
     unsigned int flag = 0;
@@ -81,12 +84,77 @@ SurakartaMove SurakartaAgentMine::CalculateMove() {
             }
         }
     }
+    for (auto& my_from : from) {
+        for (auto& opponent_to : opponent) {
+            SurakartaMove move_of_opponent = {my_from, opponent_to, (*board_)[opponent_to.x][opponent_to.y]->GetColor()};
+            SurakartaIllegalMoveReason reason_opponent = rule_manager_->JudgeMove(move_of_opponent);
+            if (reason_opponent == SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE) {
+                for (auto& my_to : to) {
+                    {
+                        int flag_eat_opponent = 0;
+                        SurakartaMove move = {my_from, my_to, game_info_->current_player_};
+                        SurakartaIllegalMoveReason reason = rule_manager_->JudgeMove(move);
+                        if (reason == SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE) {
+                            for (auto& p : opponent) {
+                                SurakartaMove move_of_opponent = {p, my_to, (*board_)[p.x][p.y]->GetColor()};
+                                (*board_)[my_to.x][my_to.y]->SetColor(game_info_->current_player_);
+                                (*board_)[my_from.x][my_from.y]->SetColor(PieceColor::NONE);
+                                SurakartaIllegalMoveReason reason_of_opponent = rule_manager_->JudgeMove(move_of_opponent);
+                                (*board_)[my_from.x][my_from.y]->SetColor(game_info_->current_player_);
+                                (*board_)[my_to.x][my_to.y]->SetColor(PieceColor::NONE);
+                                if (reason_opponent == SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE) {  // 可吃对方子且不会被吃
+                                    flag_eat_opponent = 1;
+                                }
+                            }
+                            if (flag_eat_opponent != 1)  // 如果这个时候就直接吃了吧！
+                                return move;
+                        }
+
+                        else if (reason == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE) {
+                            int flag_eat_opponent = 0;
+                            (*board_)[my_to.x][my_to.y]->SetColor(game_info_->current_player_);
+                            (*board_)[my_from.x][my_from.y]->SetColor(PieceColor::NONE);  // 先更新期盘
+                            for (auto& opponent_from : opponent) {                        // 判断移动后是否会被吃
+                                SurakartaMove move_opponent2 = {opponent_from, my_to, (*board_)[opponent_from.x][opponent_from.y]->GetColor()};
+                                SurakartaIllegalMoveReason reason_opponent = rule_manager_->JudgeMove(move_opponent2);
+                                if (reason_opponent == SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE) {
+                                    flag_eat_opponent = 1;
+                                }
+                            }
+                            (*board_)[my_to.x][my_to.y]->SetColor(PieceColor::NONE);
+                            (*board_)[my_from.x][my_from.y]->SetColor(game_info_->current_player_);
+                            if ((move.to.x == 0) + (move.to.y == 0) + (move.to.x == 5) + (move.to.y == 5) == 2) {
+                                fis_bad_move = move;
+                                continue;
+                            }
+                            flag++;
+                            if (flag_eat_opponent != 1 && flag == which_ran)
+                                none_eat_move = move;
+                            if (flag_eat_opponent != 1 && flag != which_ran)
+                                fis_ran_move = move;
+                            fis_last_move = move;
+                        }
+                    }
+                }
+            }
+        }
+    }
     SurakartaIllegalMoveReason reason1 = rule_manager_->JudgeMove(can_eat_move);
     SurakartaIllegalMoveReason reason2 = rule_manager_->JudgeMove(none_eat_move);
     SurakartaIllegalMoveReason reason3 = rule_manager_->JudgeMove(bad_move);
     SurakartaIllegalMoveReason reason4 = rule_manager_->JudgeMove(ran_move);
+    SurakartaIllegalMoveReason reason5 = rule_manager_->JudgeMove(fis_ran_move);
+    SurakartaIllegalMoveReason reason6 = rule_manager_->JudgeMove(fis_last_move);
+    SurakartaIllegalMoveReason reason7 = rule_manager_->JudgeMove(fis_bad_move);
     if (reason1 == SurakartaIllegalMoveReason::LEGAL_CAPTURE_MOVE)  // 优先级，能吃的先走
         return can_eat_move;
+
+    if (reason5 == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE)  // 不能吃的，但走了也不会被吃的
+        return fis_ran_move;
+    if (reason7 == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE)  // 最差的走法
+        return fis_bad_move;
+    if (reason6 == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE)  // 不能吃的，但走了也不会被吃的（随机版本）
+        return fis_last_move;
     if (reason2 == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE)  // 不能吃的，但走了也不会被吃的（随机版本）
         return none_eat_move;
     if (reason4 == SurakartaIllegalMoveReason::LEGAL_NON_CAPTURE_MOVE)  // 不能吃的，但走了也不会被吃的
